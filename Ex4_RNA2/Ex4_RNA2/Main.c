f5d0a48bdfac24e4901350add45c3e823c4083c6
Matheus Rossetti <matheus__rossetti@hotmail.com>
2017-08-28 22:34:10 -0300

    1.0.0.2808201702

--- Ex4_RNA2/Ex4_RNA2/Main.c
+++ Ex4_RNA2/Ex4_RNA2/Main.c
@@ -8,155 +8,142 @@
 #define ENTRADAS    3
 #define SAIDAS      2
 #define NR_AMOSTRAS 8
-#define EPOCAS      1
+#define EPOCAS      10000
 #define TX_APR      0.7
 
 int dados_treinamento[NR_AMOSTRAS][ENTRADAS + SAIDAS] = { { 0, 0, 0, 0, 0 },
 														  { 0, 0, 1, 0, 1 },
 														  { 0, 1, 0, 1, 0 },
-														  { 0, 1, 1, 1, 1 }, 
+														  { 0, 1, 1, 1, 1 },
 														  { 1, 0, 0, 0, 0 },
 														  { 1, 0, 1, 0, 1 },
 														  { 1, 1, 0, 1, 0 },
 														  { 1, 1, 1, 1, 1 } };
-
+			
 float pesos_sinapticos[SAIDAS][ENTRADAS + 1] = { { 0, 0, 0, 0 },
 												 { 0, 0, 0, 0 } }; //W
 
 //Funções auxiliares
-int		erro_1																		(int desejado, int saida);
-int		erro_2																		(int desejado, int saida);
+int		erro																		(int desejado, int saida);
 int		f_degrau																	(float saida);
-float	calcular_net_1																();
-float	calcular_net_2																();
-void	mostrar_pesos_sinapticos													(int entradas[3]);
+float	calcular_net																(int DadosEntradas[ENTRADAS], int indice);
+void	mostrar_pesos_sinapticos													(int DadosEntradas[ENTRADAS]);
 int		calcular_saida																(float net);
 void	treinar_RNA																	();
-void	ajustar_pesos_sinapticos													(int entradas[3], int erro, int indice);
+void	ajustar_pesos_sinapticos													(int DadosEntradas[ENTRADAS], int erro, int indice);
 
-int main()
+int main																			()
 {
-	int entradas[3];
+	int cont, DadosEntradas[ENTRADAS];
 
 	do
 	{
-		printf("Entrada 1: ");
-			scanf("%d", &entradas[0]);
-		printf("Entrada 2: ");
-			scanf("%d", &entradas[1]);
-		printf("Entrada 3: ");
-			scanf("%d", &entradas[2]);
+		for (cont = 0; cont < ENTRADAS; cont++)
+		{
+			printf("Entrada %i: ", cont);
+			scanf("%i", &DadosEntradas[cont]);
+		}
 
 		//TREINA A REDE
 		treinar_RNA();
 
 		//MOSTRA OS PESOS SINAPTICOS
-		mostrar_pesos_sinapticos(entradas);
-	
+		mostrar_pesos_sinapticos(DadosEntradas);
+
 		//USAR RNA
-		printf("SAIDA 1 DA RNA: %d\n", calcular_saida(calcular_net_1(entradas)));
-		printf("SAIDA 2 DA RNA: %d\n", calcular_saida(calcular_net_2(entradas)));
-	} while ((entradas[0] != -100) && (entradas[1] != -100) && (entradas[2] != -100));
+		for (cont = 0; cont < SAIDAS; cont++)
+		{
+			printf("SAIDA %i DA RNA: %d\n", cont, calcular_saida(calcular_net(DadosEntradas, cont)));
+		}
+	} while ((DadosEntradas[0] != -100) && (DadosEntradas[1] != -100) && (DadosEntradas[2] != -100));
 
 	system("pause");
 	return 0;
 }
 
-int erro_1(int desejado, int saida)
+int erro																			(int desejado, int saida)
 {
 	return desejado - saida;
 }
 
-int erro_2(int desejado, int saida)
+float calcular_net(int DadosEntradas[ENTRADAS], int indice)
 {
-	return desejado - saida;
-}
-
-int f_degrau(float saida)
-{
-	if (saida > 0) return 1;
-	return 0;
-}
-
-float calcular_net_1(int entradas[3])
-{
+	int cont;
 	float net = 0;
 	//Para Biaz
-	net = pesos_sinapticos[0][0] * 1;
-	//Para entrada 1
-	net += pesos_sinapticos[0][1] * entradas[0];
-	//Para entrada 2
-	net += pesos_sinapticos[0][2] * entradas[1];
-	//Para entrada 3
-	net += pesos_sinapticos[0][3] * entradas[2];
+	net = pesos_sinapticos[indice][0] * 1;
+	//Para entrada
+	for (cont = 0; cont < ENTRADAS; cont++)
+	{
+		net += pesos_sinapticos[indice][cont + 1] * DadosEntradas[cont - 1];
+	}
 
 	return net;
 }
 
-float calcular_net_2(int entradas[3])
-{
-	float net = 0;
-	//Para Biaz
-	net = pesos_sinapticos[1][0] * 1;
-	//Para entrada 1
-	net += pesos_sinapticos[1][1] * entradas[0];
-	//Para entrada 2
-	net += pesos_sinapticos[1][2] * entradas[1];
-	//Para entrada 3
-	net += pesos_sinapticos[1][3] * entradas[2];
-
-	return net;
-}
-
-void mostrar_pesos_sinapticos()
+void mostrar_pesos_sinapticos														()
 {
 	int i, j;
 
-	for (i = 0; i < SAIDAS; i++)
+	for (j = 0; j < SAIDAS; j++)
 	{
-		for (j = 0; j < ENTRADAS + 1; j++)
+		for (i = 0; i < (ENTRADAS + 1); i++)
 		{
-			printf("w[%d][%d]: %.2f\n", i, j, pesos_sinapticos[i][j]);
+			printf("Pesos Sinapticos[%i][%i]: %.2f\n", j, i, pesos_sinapticos[j][i]);
 		}
 	}
+	printf("\n");
 }
 
-int calcular_saida(float net)
+int calcular_saida																	(float net)
 {
 	return f_degrau(net);
 }
 
-void treinar_RNA()
+int f_degrau																		(float saida)
 {
-	int i , j, entradas[3], vlr_erro_1, vlr_erro_2, Saida1, Saida2;
-	
+	if (saida > 0)
+	{
+		return 1;
+	}
+	return 0;
+}
+
+void treinar_RNA																	()
+{
+	int i, j, k, vlr_erro[SAIDAS], Saida[SAIDAS], DadosEntradas[ENTRADAS + SAIDAS];
+
 	for (i = 0; i < EPOCAS; i++)
 	{
-		for(j = 0; j < NR_AMOSTRAS; j++)
+		for (j = 0; j < NR_AMOSTRAS; j++)
 		{
-			entradas[0] = dados_treinamento[j][0];
-			entradas[1] = dados_treinamento[j][1];
-			entradas[2] = dados_treinamento[j][2];
-			
+			for (k = 0; k < (ENTRADAS + SAIDAS); k++)
+			{
+				DadosEntradas[k] = dados_treinamento[j][k];
+			}
+
 			//CALCULAR SAIDA
-			Saida1 = calcular_saida(calcular_net_1(entradas));
-			Saida2 = calcular_saida(calcular_net_2(entradas));
-			
-			//VERIFICAÇÃO ERRO
-			vlr_erro_1 = erro_1(dados_treinamento[j][3], Saida1);
-			vlr_erro_2 = erro_2(dados_treinamento[j][3], Saida2);
-			
-			if (vlr_erro_1 && vlr_erro_2)
+			for (k = 0; k < SAIDAS; k++)
+			{
+				Saida[k] = calcular_saida(calcular_net(DadosEntradas, k));
+			}
+
+			//VERIFICAÇÃO ERRO 0 1 2 3 4
+			for (k = 0; k < SAIDAS; k++)
+			{
+				vlr_erro[k] = erro(DadosEntradas[ENTRADAS + k], Saida[k]);
+			}
+
+			for (k = 0; k < SAIDAS; k++)
 			{
 				//Ajusta pesos sinapticos
-				ajustar_pesos_sinapticos(entradas, vlr_erro_1, 0);
-				ajustar_pesos_sinapticos(entradas, vlr_erro_2, 1);
+				ajustar_pesos_sinapticos(DadosEntradas, vlr_erro[k], k);
 			}
 		}
 	}
 }
 
-void ajustar_pesos_sinapticos(int entradas[3], int erro, int indice)
+void ajustar_pesos_sinapticos														(int DadosEntradas[ENTRADAS], int erro, int indice)
 {
 	int i;
 
@@ -165,6 +152,6 @@ void ajustar_pesos_sinapticos(int entradas[3], int erro, int indice)
 
 	for (i = 1; i < ENTRADAS + 1; i++)
 	{
-		pesos_sinapticos[indice][i] = pesos_sinapticos[indice][i] + TX_APR * erro * entradas[i - 1];
+		pesos_sinapticos[indice][i] = pesos_sinapticos[indice][i] + TX_APR * erro * DadosEntradas[i - 1];
 	}
 }
